1f5b5c00165b5582bc0d7f8d875db283d6b733ab
Marcelo Janazi <marcelo.janazi@superdigital.com.br>
2021-06-16 20:35:20 -0300

    including dtos

---
+++ templates/TemplateMicroservice/MicroserviceBase/ApiError.cs
@@ -0,0 +1,23 @@
+﻿using Flunt.Notifications;
+using MicroserviceBase.Application;
+using System.Collections.Generic;
+
+namespace MicroserviceBase
+{
+    public class ApiError
+    {
+        public ApiError(IEnumerable<Notification> errors, ErrorCode? error = null)
+        {
+            ErrorType = error;
+            Errors = errors;
+        }
+
+        public ErrorCode? ErrorType { get; }
+        public IEnumerable<Notification> Errors { get; }
+
+        public static ApiError FromResult(Result result)
+        {
+            return new(result.Notifications, result.Error);
+        }
+    }
+}
